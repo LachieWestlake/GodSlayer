ca2b35b329b1bf6ef9ca7b0c1e5232b5a878e067
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-07-13 21:15:20 +1000

    Added enemy health scripts and hitscan damage scripts and testing

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public int health = 100;
+
+    private void DeductPoints(int damageAmount)
+    {
+        health -= damageAmount;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HitscanDamageScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class HitscanDamageScript : MonoBehaviour
+{
+    public int damageAmount = 1;
+    public float targetDistance;
+    public float allowedRange = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            RaycastHit shot;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+            {
+                targetDistance = shot.distance;
+
+                if (targetDistance <= allowedRange)
+                {
+                    shot.transform.SendMessage("DeductPoints", damageAmount, SendMessageOptions.DontRequireReceiver);
+                    Debug.Log("Hit for " + damageAmount.ToString());
+                }
+            }
+        }
+    }
+}
