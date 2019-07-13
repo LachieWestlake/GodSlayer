21758f51ef8c1585ed8304fc0a18fdcd60ee7551
LACHLAN WESTLAKE <102109556@student.swin.edu.au>
2019-07-13 21:29:55 +1000

    Picking up Objects ("E")

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public Transform onHand;
+
+    void Update()
+    {
+        //Hit E, pickup object
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+                GetComponent<Rigidbody>().isKinematic = true;
+                GetComponent<Rigidbody>().useGravity = false;
+                this.transform.position = onHand.transform.position;
+                this.transform.parent = GameObject.Find("FPSController").transform;
+                this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+         
+        }
+
+        //Release E, drop object
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
