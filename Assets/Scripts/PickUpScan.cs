6b730bdd3e6929d4501e9bfb9f75de79355e8cdb
Dominic Codespoti <33775298+thed24@users.noreply.github.com>
2019-07-14 21:03:10 +1000

    Re-coded pick up mechanic to utilize raycasting

---
+++ Assets/Scripts/PickUpScan.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PickUpScan : MonoBehaviour
+{
+    public bool currentlyPickedUp;
+    public float targetDistance;
+    public float allowedRange = 15;
+    public RaycastHit pickUpRaycastHit;
+    public Transform onHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && currentlyPickedUp)
+        {
+            pickUpRaycastHit.collider.gameObject.transform.parent = null;
+            pickUpRaycastHit.collider.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            pickUpRaycastHit.collider.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            currentlyPickedUp = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out pickUpRaycastHit) && currentlyPickedUp == false)
+            {
+                targetDistance = pickUpRaycastHit.distance;
+
+                if (targetDistance <= allowedRange)
+                {
+                    pickUpRaycastHit.collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                    pickUpRaycastHit.collider.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                    pickUpRaycastHit.collider.gameObject.transform.position = new Vector3(onHand.transform.position.x, onHand.transform.position.y , onHand.transform.position.z); 
+                    pickUpRaycastHit.collider.gameObject.transform.parent = GameObject.Find("FPSController").transform;
+                    pickUpRaycastHit.collider.gameObject.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+                    currentlyPickedUp = true;
+                }
+            }
+        }
+    }
+}
