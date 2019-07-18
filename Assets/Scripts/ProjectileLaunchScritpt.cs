a13de9483025bab059031c34cd565f39b95d630c
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-07-18 21:59:47 +1000

    Player can shoot a simple projectile from the crosshair in the direction of the target

---
+++ Assets/Scripts/ProjectileLaunchScritpt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ProjectileLaunchScritpt : MonoBehaviour
+{
+    public int damageAmount;
+    public float speed;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Debug.Log(transform.position);
+            Rigidbody p = Instantiate(projectile, transform.position, transform.rotation).GetComponent<Rigidbody>();
+            p.velocity = transform.forward * speed;
+        }
+    }
+}
